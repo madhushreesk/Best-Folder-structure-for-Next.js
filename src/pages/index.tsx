3fb23ddb06b946ddaa4551d6115ec82e44a8e4a2
Madhushri S. Kulkarni <88674169+madhushreesk@users.noreply.github.com>
2023-06-01 13:07:59 +0530

    vscode configuration added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,11 @@ const index = () => {
   return (
     <div>
       <h2>Project setup</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit lectus nec dui posuere,
+         sit amet ullamcorper dolor tristique. Proin sagittis, dui non consectetur gravida, neque purus congue est,
+          ut tristique purus augue a risus. Aliquam id varius leo. Donec vehicula dui eget enim feugiat, ut fermentum 
+          lorem tincidunt. Nam commodo tellus sit amet facilisis tincidunt. Sed sit amet augue ac lectus rhoncus hendrerit. 
+          Nulla facilisi. Curabitur et ultrices enim. Nunc id lectus elit.</p>
     </div>
   );
 };
